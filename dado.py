6290c0791a431fdfbb91bf3fa96e6941ef73b167
JessicaFerraz13 <jessicaferrazrosario@gmail.com>
2023-02-17 21:40:47 -0300

    update_janela

--- dado.py
+++ dado.py
@@ -6,23 +6,30 @@ class dado():
         self.valorMinimo = 1
         self.valorMaximo = 6
         self.layout = [
-            [sg.Text("Jogar o Dado?")],
-            [sg.Button("Sim"), sg.Button("Não")]
+            [sg.Text("      Simulador de dado      ", font = "Arial 25")],
+            [sg.Text("")],
+            [sg.Text("-", font = "Arial 70", key = "valor_dado")],
+            [sg.Text("")],
+            [sg.Button("    Jogar!    ", font = "Arial 15")],
+            [sg.Text("")]          
         ]
+        self.janela = sg.Window("Simulador de dado", layout = self.layout, element_justification = "c")
         
     def iniciar(self):
-        self.janela = sg.Window("Simulador de dado", layout = self.layout)
-        self.eventos, self.valores = self.janela.Read()
-        try:
-            if self.eventos == "Sim":
-                self.gerarValorDado()
-            elif self.eventos == "Não":
-                print("Obrigada por utilizar nossa aplicação!")
-        except:
-            print("Erro")
+        while True:
+            eventos, valores = self.janela.Read()
+            try:
+                if eventos == "    Jogar!    ":
+                    valor = self.gerarValorDado()
+                    self.janela["valor_dado"].update(valor)
+                elif eventos == sg.WIN_CLOSED:
+                    break
+            
+            except:
+                print("Erro")
         
     def gerarValorDado(self):
-        print(random.randint(self.valorMinimo, self.valorMaximo))
+        return random.randint(self.valorMinimo, self.valorMaximo)
 
 Dado = dado()
 Dado.iniciar()
